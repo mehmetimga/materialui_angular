944c09461b067063c1ff101511bd801a533d4d81
mehmetimga <imga@rushstreetinteractive.com>
2023-04-15 21:46:10 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { LessonButton } from './components/LessonButton';
+import { LessonTypography } from './components/LessonTypography';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LessonTypography/> */}
+      <LessonButton />
+    </div>
+  );
+}
+
+export default App;
